30ee1c212470d860ff6140507a2c51b6e81b5724
DiegoOliv07 <diego.oliv2007@gmail.com>
2023-12-22 00:05:30 -0300

    Create 1002.java

---
+++ Beecrowd/1002.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    static double pi = 3.14159;
+    static double area(double raio){
+        // area = π . raio2
+
+        double area = pi * Math.pow(raio, 2);
+        return area;
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        double raioo = scanner.nextDouble();
+        System.out.printf("A=%.4f%n", area(raioo));
+    }
+ 
+}
