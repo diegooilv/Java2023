dc1e9f6b784a2ae0223d7d0edf4cfced607287a3
DiegoOliv07 <diego.oliv2007@gmail.com>
2023-12-22 00:05:21 -0300

    Create 1007.java

---
+++ Beecrowd/1007.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    static int dif(int A, int B, int C, int D){
+        return (A * B - C * D);
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int va1 = scanner.nextInt();
+        int va2 = scanner.nextInt();
+        int va3 = scanner.nextInt();
+        int va4 = scanner.nextInt();
+        System.out.printf("DIFERENÇA = %d%n",dif(va1, va2, va3, va4));
+ 
+    }
+ 
+}
